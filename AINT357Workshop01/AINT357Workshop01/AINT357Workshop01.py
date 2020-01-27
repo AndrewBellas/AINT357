6f34c68085ad56dd01bb49f65b51665570d405a1
(s) Andrew Bellas <andrew.bellas@students.plymouth.ac.uk>
2020-01-27 12:53:27 +0000

    Added more explanations to Python script demo

--- AINT357Workshop01/AINT357Workshop01/AINT357Workshop01.py
+++ AINT357Workshop01/AINT357Workshop01/AINT357Workshop01.py
@@ -93,6 +93,84 @@ def calculateSquare(numToSquare):
 
 print(calculateSquare(2))
 
+#7. Lists, arrays, dictionaries, tuples, sets:
+
+#Lists
+#Declaring a list:
+newList = ["Car", "Pen", "Spoon"]
+
+#Print all items in list:
+print(newList)
+
+#Print specific item in list:
+print(newList[0])
+
+#====================
+#TODO: ADD MORE HERE
+#====================
+
+#8. I/O to/from console:
+
+#Input:
+storedInput = input("OPTIONAL: A string can appear here to prompt input!")
+
+#Output:
+print(storedInput) 
+
+#9. I/O to/from files:
+
+#================================
+#TODO: MAYBE ENRICH THIS SECTION
+#================================
+
+#Taking input from a file:
+#(Commented out so as not to create the file currently)
+#streamVar = open("someTextDocument.txt", w+) #Takes 2 arguments, the text document to manipulate and the operations we wish to do on it. The potential options are:
+
+# 'w' = Allows us to write to the file (Will create the file if it does not already exist)
+# 'w+' = Allows us to read and write to the file (Will create the file if it does not already exist)
+# 'r' = Allows us to read from the file (Error if the file doesn't exist)
+# 'a' = Allows us to append the file (creates the file if it doesn't already exist)
+# 'x' = Creates the file if it doesn't already exist (Error if it does)
+
+#10. Modules:
+
+#Simply put, a module is just a group of Python code. Much like a package in Java, it can define functions, classes, and variables.
+#Python modules can be imported, for example:
+
+import math #Imported the math module
+
+a = math.factorial(3) #Finds the factorial of 3
+print(a)
+
+#11. Classes/OO:
+
+#Classes in Python act much like classes in other programming languages; they are templates from which an object can be created:
+
+#Simple class with variables, constructor and method
+
+class MyClass: #Class definition
+
+    x = 5 #variables/field
+    y = 0 #variables/field
+
+    def __init__(self): #Constructor
+        self.y = 9 #Set value of field to 9
+
+    def compare(a, b): #Compare method
+        if (a == b):
+            print("They are the same number")
+        elif (a < b):
+            print("The first number is smaller than the second")
+        elif (a > b):
+            print("The first number is bigger than the second")
+
+
+#Instancing the class:
+
+objectOne = MyClass
+
+print(objectOne.compare(9, 9))
 
 
 
